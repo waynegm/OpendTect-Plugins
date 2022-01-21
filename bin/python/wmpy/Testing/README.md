bca88f27fd20bb564c01039033f207e56945e1bb
waynegm <wayne.mogg@gmail.com>
2022-01-21 18:22:11 +1000

    Add ex_test_gradient.py script

---
+++ bin/python/wmpy/Testing/README.md
@@ -0,0 +1,8 @@
+# External Attribute Scripts to Create Test Data
+This a collection of [External Attribute](http://waynegm.github.io/OpendTect-Plugin-Docs/Attributes/ExternalAttrib/) scripts create
+artificial data used to test other attributes in the wmPlugins.
+
+| PLUGIN | DESCRIPTION |
+|--------|-------------|
+| ex_test_gradient.py | Generate a synthetic gradient volume |
+
---
+++ bin/python/wmpy/Testing/ex_test_gradient.py
@@ -0,0 +1,43 @@
+# Generate Test Gradient
+#
+# Input: Intercept volume
+# Output: Gradient volume for given crossplot angle
+#
+import sys,os
+import numpy as np
+import math
+#
+# Import the module with the I/O scaffolding of the External Attribute
+#
+sys.path.insert(0, os.path.join(sys.path[0], '..'))
+import extattrib as xa
+
+#
+# The attribute parameters - keep what you need
+#
+xa.params = {
+	'Inputs': ['Intercept'],
+	'Angle (deg)' : {'Type': 'Number', 'Value': 30},
+	'Parallel' : False,
+	'Help'  : 'http://waynegm.github.io/OpendTect-Plugin-Docs/Attributes/ExternalAttrib/'
+}
+#
+# Define the compute function
+#
+def doCompute():
+	angle = xa.params['Angle (deg)']['Value']
+	factor = math.tan( angle/180 * math.pi )
+	while True:
+		xa.doInput()
+		indata = xa.Input['Intercept'][0,0,:]
+		outdata = indata * factor
+		xa.Output = outdata
+		xa.doOutput()
+
+xa.doCompute = doCompute
+#
+# Do it
+#
+xa.run(sys.argv[1:])
+
+
