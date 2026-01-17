079379f4c437fca80a4611e81b152d0a17e007c4
waynegm <wayne.mogg@gmail.com>
2026-01-17 12:24:52 +1000

    remove old style include guards

--- plugins/Mistie/mistiedata.h
+++ plugins/Mistie/mistiedata.h
@@ -1,5 +1,4 @@
-#ifndef mistiedata_h
-#define mistiedata_h
+#pragma once
 
 #include "mistiemod.h"
 #include "bufstringset.h"
@@ -11,22 +10,22 @@ mExpClass(Mistie) MistieData
 {
 public:
     MistieData();
-    MistieData( const MistieData& d) { *this = d; } 
+    MistieData( const MistieData& d) { *this = d; }
     ~MistieData();
-    
+
     MistieData& operator=(const MistieData&);
-    
+
     static const char* extStr()     { return "mistie"; }
     static const char* filtStr()    { return "*.mistie"; }
     static const char* defDirStr()  { return "Misc"; }
-    
+
     bool            read( const char* filename, bool merge=false, bool replace=false );
     bool            write( const char* filename ) const;
     int             size() const;
     void            erase();
-    
+
     void            add( const MistieData& other );
-    bool            add( const char* dataA, int trcA, const char* dataB, int trcB, Coord pos, 
+    bool            add( const char* dataA, int trcA, const char* dataB, int trcB, Coord pos,
                          float zdiff, float phasediff, float ampdiff , float quality, bool replace=false );
     bool            add( const char* dataA, int trcA, const char* dataB, int trcB, Coord pos );
 
@@ -43,16 +42,16 @@ public:
     bool            get( int idx, BufferString& dataA, int& trcA, BufferString& dataB, int& trcB ) const;
     bool            getLines( int idx, BufferString& dataA, BufferString& dataB ) const;
     bool            get( int idx, BufferString& dataA, BufferString& dataB, float& zdiff, float& phasediff, float& ampdiff ) const;
-    bool            get( int idx, BufferString& dataA, int& trcA, BufferString& dataB, int& trcB, Coord& pos, 
+    bool            get( int idx, BufferString& dataA, int& trcA, BufferString& dataB, int& trcB, Coord& pos,
                          float& zdiff, float& phasediff, float& ampdiff, float& quality ) const;
-    bool            getWith( const MistieCorrectionData& corrections, int idx, 
+    bool            getWith( const MistieCorrectionData& corrections, int idx,
                                           float& zdiff, float& phasediff, float& ampdiff ) const;
-    
+
     bool            setPhaseMistie(int idx, float phasediff);
-    bool            set( int idx, const char* dataA, int trcA, const char* dataB, int trcB, Coord pos, 
+    bool            set( int idx, const char* dataA, int trcA, const char* dataB, int trcB, Coord pos,
                          float zdiff, float phasediff, float ampdiff , float quality );
     bool            set( int idx, float zdiff, float phasediff, float ampdiff, float quality );
-    
+
 protected:
     BufferStringSet     dataA_;
     TypeSet<int>        trcA_;
@@ -64,5 +63,3 @@ protected:
     TypeSet<float>      ampdiff_;
     TypeSet<float>      quality_;
 };
-
-#endif
